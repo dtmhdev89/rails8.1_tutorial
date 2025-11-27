4fa39f8cd0dd59f844def7861c76f1bb0e6c9ddd
Dinh Tran Minh Hieu <dtmhqng2906@gmail.com>
2025-11-27 04:27:22 +0000

    stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["tooltip"]
   connect() {
-    this.element.textContent = "Hello World!"
+    console.log("Hello world")
+  }
+
+  show_tooltip() {
+    console.log("Name tooltips")
+    this.tooltipTarget.classList.remove("hidden") 
+    this.tooltipTarget.classList.add("visible")
+  }
+
+  hide_tooltip() {
+    this.tooltipTarget.classList.remove("visble")
+    this.tooltipTarget.classList.add("hidden")
   }
 }
