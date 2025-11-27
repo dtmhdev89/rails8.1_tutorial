2e2c4e56ac138142319336121ec40cb879913dfb
Dinh Tran Minh Hieu <dtmhqng2906@gmail.com>
2025-11-27 04:47:43 +0000

    try unittest

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include ActionMailer::TestHelper
+
+  test "send email notifications when back in stock" do
+    product = products(:tshirt)
+
+    product.update(inventory_count: 0)
+
+    assert_emails 1 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
